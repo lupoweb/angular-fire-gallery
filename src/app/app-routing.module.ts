dd0b464e3d5fc1982d9bced2c2644eecac7acafd
Francesco Lupano <lupano.web90@gmail.com>
2021-07-14 13:02:32 +0200

    v

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './features/admin/admin.component';
+import { ViewerComponent } from './features/viewer/viewer.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'admin', component: AdminComponent },
+  { path: 'viewer', component: ViewerComponent },
+  { path: '**', redirectTo: 'admin' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
